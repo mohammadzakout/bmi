020b088b0eb220a2f1c60f910932b201f338ce50
hamada <mohammadzakout10@gmail.com>
2021-10-29 19:40:11 +0300

    week 1+2

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BMI Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Image.asset(
+        'assets/me2.jpeg',
+        height: MediaQuery.of(context).size.height,
+        fit: BoxFit.fill,
+      ),
+      floatingActionButton: TextButton(
+        child: const Text(
+          "Next",
+          style: TextStyle(fontSize: 30, color: Colors.white),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+}
